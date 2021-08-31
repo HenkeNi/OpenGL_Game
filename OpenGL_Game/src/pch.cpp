e4d96d0fdcfb7a4de196d1da210cef7c61d49024
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-01 00:22:22 +0200

    pre-compiled header

---
+++ OpenGL_Game/src/pch.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+
+// NOTE: this file is needed...
+
+/*
+*	For files that don't need (or shouldn't) have a precompiled header included
+*		- right click .cpp file and select properties
+*		- under C/C++ -> PreCompiled Header -> PreCompiled Header, set it to 'Not using precompiled header'
+*/
---
+++ OpenGL_Game/src/pch.h
@@ -0,0 +1,36 @@
+#pragma once
+
+/*
+*	Pre-Compiled Header 
+*		Only include things that are never (or rarely) modified
+*/
+
+#include <algorithm>
+#include <cassert>
+#include <functional>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <optional>
+#include <sstream>
+#include <stdexcept>
+#include <thread>
+#include <utility>
+
+
+
+// data structures
+#include <array>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+
+// math library
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
