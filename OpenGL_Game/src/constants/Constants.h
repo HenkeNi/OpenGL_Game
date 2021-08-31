0282a35123a453e0f742d4811a1e43a85902de9e
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-01 00:23:50 +0200

    namespace for constants

---
+++ OpenGL_Game/src/constants/Constants.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace constants
+{
+	inline constexpr int GL_VERSION_MAJOR{ 3 };
+	inline constexpr int GL_VERSION_MINOR{ 3 };
+	inline constexpr double FRAMES_PER_SECOND{ 1.0 / 60.0 };
+
+
+	inline constexpr int SCR_WIDTH{ 800 };
+	inline constexpr int SCR_HEIGHT{ 600 };
+	inline constexpr const char* title{ "Game Of OpenGL" };
+}
