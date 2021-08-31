0cb450ffb0b1e9d43f67ada5b33bfe35b5b60c3c
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-01 00:26:31 +0200

    added Texture & Shader references to class

--- OpenGL_Game/src/GameObject.cpp
+++ OpenGL_Game/src/GameObject.cpp
@@ -2,6 +2,16 @@
 #include "GameObject.h"
 
 
+
+
+
+void GameObject::draw(Renderer& renderer) const
+{
+	//renderer.draw()
+}
+
+
+
 void GameObject::setPosition(glm::vec3 position)
 {
 	m_position = position;
@@ -10,4 +20,14 @@ void GameObject::setPosition(glm::vec3 position)
 void GameObject::setSize(glm::vec3 size)
 {
 	m_size = size;
+}
+
+void GameObject::setVelocity(glm::vec3 velocity)
+{
+	m_velocity = velocity;
+}
+
+void GameObject::setRotation(float rotation)
+{
+	m_rotation = rotation;
 }
--- OpenGL_Game/src/GameObject.h
+++ OpenGL_Game/src/GameObject.h
@@ -1,16 +1,21 @@
 #pragma once
 
 #include "Texture.h"
+#include "Renderer.h"
 
 class GameObject
 {
 public:
+	void draw(Renderer& renderer) const;
 
 	void setPosition(glm::vec3 position);
 	void setSize(glm::vec3 size);
+	void setVelocity(glm::vec3 velocity);
+	void setRotation(float rotation);
 
 private:
-	Texture		m_texture;
+	Shader&		m_shader;
+	Texture&	m_texture;
 	glm::vec3   m_position{ 0.f, 0.f, 0.f }, m_size{ 1.f, 1.f, 1.f }, m_velocity{ 0.f, 0.f, 0.f };
 	float		m_rotation{ 0.f };
 };
