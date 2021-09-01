f00d197a132a1b04e729287e9d7fd59ea268401e
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-01 07:19:05 +0200

    storing VAO, and added new constructor

--- OpenGL_Game/src/GameObject.h
+++ OpenGL_Game/src/GameObject.h
@@ -6,6 +6,7 @@
 class GameObject
 {
 public:
+	GameObject(Shader& shader, Texture& texture, glm::vec3 position);
 	void draw(Renderer& renderer) const;
 
 	void setPosition(glm::vec3 position);
@@ -14,9 +15,14 @@ public:
 	void setRotation(float rotation);
 
 private:
+	void initData();
+
+private:
 	Shader&		m_shader;
 	Texture&	m_texture;
 	glm::vec3   m_position{ 0.f, 0.f, 0.f }, m_size{ 1.f, 1.f, 1.f }, m_velocity{ 0.f, 0.f, 0.f };
 	float		m_rotation{ 0.f };
+
+	unsigned int m_VAO;
 };
 
