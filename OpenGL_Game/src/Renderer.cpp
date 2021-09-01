d1c63fc2956fbcfa466c920d01df4484175faa63
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-01 15:59:36 +0200

    changed function decleration for draw()

--- OpenGL_Game/src/Renderer.cpp
+++ OpenGL_Game/src/Renderer.cpp
@@ -7,30 +7,47 @@
 //{
 //}
 
+Renderer::Renderer()
+{
+	//initRenderData();
+}
 
-void Renderer::draw(Shader& shader, const Texture& texture, glm::vec3 position, glm::vec3 size, float rotate)
+
+void Renderer::draw(Shader& shader, const Texture& texture, glm::vec3 position, glm::vec3 size, float rotate, unsigned VAO, int indices)
 {
 	shader.use();
-	texture.bind(0);
+	glBindVertexArray(VAO);
 
-	glm::mat4 view		 = glm::mat4(1.f); // intialize to identity matrix
-	glm::mat4 projection = glm::mat4(1.f);
 
-	projection = glm::perspective(glm::radians(45.f), static_cast<float>(constants::SCR_WIDTH) / static_cast<float>(constants::SCR_HEIGHT), 0.1f, 100.f);
-	view	   = glm::translate(view, glm::vec3(0.f, 0.f, -3.f));
+	// create transformations
+	//glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+	//transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
 
-	shader.setMatrix4("projection", projection);
-	shader.setMatrix4("view", view);
+	//shader.use();
+	//shader.setMatrix4("transform", transform);
 
-	glBindVertexArray(m_VAO);
-	glm::mat4 model = glm::mat4(1.f);
-	model = glm::translate(model, position);
+	//glm::mat4 view		 = glm::mat4(1.f); // intialize to identity matrix
+	//glm::mat4 projection = glm::mat4(1.f);
+
+	//projection = glm::perspective(glm::radians(45.f), static_cast<float>(constants::SCR_WIDTH) / static_cast<float>(constants::SCR_HEIGHT), 0.1f, 100.f);
+	//view	   = glm::translate(view, glm::vec3(0.f, 0.f, -3.f));
+
+	//shader.setMatrix4("projection", projection);
+	//shader.setMatrix4("view", view);
+
+	//glBindVertexArray(VAO);
+	//glm::mat4 model = glm::mat4(1.f);
+	//model = glm::translate(model, position);
 	
-	shader.setMatrix4("model", model);
-	glDrawElements(GL_TRIANGLES, m_numOfIndices, GL_UNSIGNED_INT, 0);
+	//shader.setMatrix4("model", model);
+
+	//texture.bind(0);
+
+
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 	//glm::mat4 model 
-
+	glBindVertexArray(0);
 
 
 	// bind shader
@@ -50,8 +67,47 @@ void Renderer::clearScreen()
 
 
 
-
-void Renderer::initRenderData()
-{
-
-}
+//
+//void Renderer::initRenderData()
+//{
+//	unsigned int VBO, EBO;
+//
+//	float vertices[] = {
+//		// pos      // tex
+//		 0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
+//		 0.5f, -0.5f, 0.0f,     1.0f, 0.0f, // bottom right
+//		-0.5f, -0.5f, 0.0f,     0.0f, 0.0f, // bottom left
+//		-0.5f,  0.5f, 0.0f,      0.0f, 1.0f  // top left 
+//	};
+//
+//	unsigned int indices[]
+//	{
+//		0, 1, 3, // first triangle
+//		1, 2, 3  // second triangle
+//	};
+//
+//	m_numOfIndices = sizeof(indices) / sizeof(indices[0]);
+//
+//	glGenVertexArrays(1, &m_VAO);
+//	glGenBuffers(1, &VBO);
+//	glGenBuffers(1, &EBO);
+//
+//	glBindVertexArray(m_VAO);
+//	
+//	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+//
+//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+//	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+//
+//	glEnableVertexAttribArray(0);
+//	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+//
+//	glEnableVertexAttribArray(1);
+//	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+//
+//	glBindBuffer(GL_ARRAY_BUFFER, 0);
+//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+//	glBindVertexArray(0);
+//
+//}
--- OpenGL_Game/src/Renderer.h
+++ OpenGL_Game/src/Renderer.h
@@ -8,11 +8,11 @@
 class Renderer
 {
 public:
-	Renderer() = default;
+	Renderer();
 	//Renderer(Shader& shader, Texture& texture);
 
 	//void draw(glm::vec2 position, glm::vec2 size, float rotation, glm::vec3 color = glm::vec3{ 1.f });
-	void draw(Shader& shader, const Texture& texture, glm::vec3 position, glm::vec3 size = glm::vec3(10.f, 10.f, 10.f), float rotate = 0.0f);
+	void draw(Shader& shader, const Texture& texture, glm::vec3 position, glm::vec3 size, float rotate, unsigned VAO, int indices);
 
 	//void draw(Shader& shader, Texture& texture, int numOfIndices, unsigned int VAO, const glm::vec3& position); // Take in VAO, EBO?? -- take in a Material instead of a Shader?? (Material = shader + uniforms??)
 
@@ -20,11 +20,11 @@ public:
 	void		clearScreen();
 
 private:
-	void initRenderData();
+	//void initRenderData();
 
 private:
-	unsigned m_VAO;
-	unsigned m_numOfIndices;
+	//unsigned m_VAO;
+	//unsigned m_numOfIndices;
 	//Shader& m_shader;
 	//Texture& m_texture;
 };
